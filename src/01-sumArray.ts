b3669bf80117d06b88d8184982e3091beef7dc44
Alamin-22 <mdalaminmollick@gmail.com>
2024-11-07 23:21:42 +0600

    Problem 3 Resolved

--- src/01-sumArray.ts
+++ src/01-sumArray.ts
@@ -6,8 +6,9 @@
     for (const num of arr) {
       totalSum += num;
     }
-    console.log(totalSum);
     return totalSum;
   };
-  sumArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const res1 = sumArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  console.log({ res1 });
 }
---
+++ src/03-countWordOccurrences.ts
@@ -0,0 +1,26 @@
+{
+  // Create a TypeScript function called countWordOccurrences that accepts a sentence (string) and a word (string). The function should return the number of times the word appears in the sentence, ignoring case. Use the simple sentence without punctuation as input.
+
+  const countWordOccurrences = (sentence: string, CountableWord: string) => {
+    const ArrOfWord: Array<string> = sentence
+      .trim()
+      .toLocaleLowerCase()
+      .split(/\s+/); // split by space
+
+    let countedWord = 0;
+    for (const word of ArrOfWord) {
+      if (word === CountableWord.toLowerCase()) {
+        countedWord++;
+      }
+    }
+    return countedWord;
+  };
+
+  const res1 = countWordOccurrences(
+    "Currently I am working On my first assignment of Level 2 course I have to practice more and more These assignment are soo much important So I have to focus on the Assignment",
+    "assignment"
+  );
+
+  console.log({res1});
+
+}
